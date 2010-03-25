114bbd41993c45424bd508f0813509f9c0e8de62
Ryan Weaver <ryan@thatsquality.com>
2010-03-24 21:38:30 -0500

    Adding a few other i18n related things (and some chmod nonsense0

---
+++ lib/filter/doctrine/sfSympalBlogPlugin/sfSympalBlogPostTranslationFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sfSympalBlogPostTranslation filter form.
+ *
+ * @package    thatsquality
+ * @subpackage filter
+ * @author     ryan@thatsquality.com
+ * @version    SVN: $Id: sfDoctrinePluginFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfSympalBlogPostTranslationFormFilter extends PluginsfSympalBlogPostTranslationFormFilter
+{
+  public function configure()
+  {
+  }
+}
